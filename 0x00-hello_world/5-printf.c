6db5ca7a942fb0d96b327f0fb3334f02d466fc2b
Marty-A <henkashasmah@gmail.com>
2022-12-08 16:58:12 -0800

    c_lang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main -Printing
- *Return -Always 0
+ *main - Beginning
+ *Return - Always 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

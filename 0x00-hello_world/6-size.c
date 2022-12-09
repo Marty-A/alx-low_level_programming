3065f8c1048dc97aa269c1d12913000e906b7fb1
Marty-A <henkashasmah@gmail.com>
2022-12-08 18:55:20 -0800

    c_lang

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
-	'\n'; 
+	float e;	
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
 	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));

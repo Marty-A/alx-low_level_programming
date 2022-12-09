9a8b77409c4fd2ee68cc41526db07b42422405e2
Marty-A <henkashasmah@gmail.com>
2022-12-08 16:38:53 -0800

    c_lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - Entry
+ * main - Beginning
  * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+	return(0);
 }

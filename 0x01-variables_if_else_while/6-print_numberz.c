b7aa65d96fa6e043061b7b364a42eafde3c08d11
Marty-A <henkashasmah@gmail.com>
2022-12-12 07:52:05 -0800

    c_var

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print base 10 numbers starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	char y;
+
+	while (y = 0 && y <= 9 && y++)
+	putchar(y);
+	putchar('\n');
+
+	return (0);
+}

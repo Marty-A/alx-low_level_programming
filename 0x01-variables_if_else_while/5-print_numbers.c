827d3ad661bcf03b9ab953b3706d07d54e0cf8ff
Marty-A <henkashasmah@gmail.com>
2022-12-12 07:37:20 -0800

    c_var

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print single digit base 10 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	while (x = 0 && x < 10)
+	printf("%d \n", x);
+
+	return (0);
+}

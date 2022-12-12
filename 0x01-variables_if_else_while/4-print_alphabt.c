b9b29c1fffa805db785aaba295a5736d1d0f1b41
Marty-A <henkashasmah@gmail.com>
2022-12-12 03:10:59 -0800

    c_var

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print alphadets with exceptions in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'a' ; y < 'e' ; y >= 'f')
+	for (y < 'q' ; y >= 'r' ;)
+
+	putchar(y);
+	putchar('\n');
+
+	return (0);
+}

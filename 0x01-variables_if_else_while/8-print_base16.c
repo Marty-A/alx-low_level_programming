434dde42f8ed97956983f8e9eb23520e1899d447
Marty-A <henkashasmah@gmail.com>
2022-12-12 10:12:41 -0800

    c_var

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print base 16 numbers in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0 ; x <= 9 ; x++)
+	putchar(x);
+	for (y = 'a' ; y <= 'f' ; y++)
+	putchar(y);
+	putchar('\n');
+
+	return (0);
+}

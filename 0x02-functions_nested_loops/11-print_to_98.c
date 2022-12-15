56a572e02fe8f62cf35e3bb21d92ed0567cb062d
Marty-A <henkashasmah@gmail.com>
2022-12-14 22:58:41 -0800

    c_func

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,16 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98 && n++)
+	while (n < 98)
 	{
 	printf("%i, ", n);
+	n++;
 	}
 
-	while (n > 98 && n--)
+	while (n > 98)
 	{
 	printf("%i, ", n);
+	n--;
 	}
 
 	printf("98");

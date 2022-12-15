f1450aafb792de2e53144b0a12bbe32c91c19a3c
Marty-A <henkashasmah@gmail.com>
2022-12-14 20:50:43 -0800

    c_func

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * @n: number to check
  * Return: 1 or 0 or -1
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	_putchar('+');
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value
+ * @n: integer
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+
+	else
+	return (-n);
+}

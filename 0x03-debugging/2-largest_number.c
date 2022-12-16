db98cc7749203335d1b89b9b3cb49de00c1326c6
Marty-A <henkashasmah@gmail.com>
2022-12-16 15:54:24 -0800

    c_debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,28 +8,28 @@
 
 int main(void)
 {
-    int a, b, c;
-    int largest;
+	int a, b, c;
+	int largest;
 
-    largest = largest_number(a, b, c);
+	largest = largest_number(a, b, c);
 
-    do printf("%d is the largest number\n", largest);
-    while (a > b && a > c);
-    {
-        largest = a;
-    }
+	do printf("%d is the largest number\n", largest);
+	while (a > b && a > c);
+	{
+		largest = a;
+	}
 
-    do printf("%d is the largest number\n", largest);
-    while (b > a && b > c);
-    {
-        largest = b;
-    }
+	do printf("%d is the largest number\n", largest);
+	while (b > a && b > c);
+	{
+		largest = b;
+	}
 
-    do printf("%d is the largest number\n", largest);
-    while (c > a && c > b);
-    {
-        largest = c;
-    }  
+	do printf("%d is the largest number\n", largest);
+	while (c > a && c > b);
+	{
+		largest = c;
+	}
 
-    return (0);
+	return (0);
 }

966f35233fe54c1616be45b8fe6bcfdfd5106a18
Marty-A <henkashasmah@gmail.com>
2022-12-19 18:45:29 -0800

    more_f

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
-void print_most_ numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);

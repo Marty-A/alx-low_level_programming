92c121fd03406cba7203b0715f832c89531f8b18
Marty-A <henkashasmah@gmail.com>
2022-12-22 17:56:55 -0800

    p_a_s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,14 @@ char *_strcat(char *dest, char *src)
 	int length, x;
 
 	length = 0;
-	dest[length] = '\0';
 
 	while (dest[length] != '\0')
 		length++;
 
 	for (x = 0 ; src[x] != '\0' ; x++, length++)
 		dest[length] = src[x];
+	
+	dest[length] = '\0';
 
 	return (dest);
 }

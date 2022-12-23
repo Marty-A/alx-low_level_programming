72729e9774803f7d175c66abf2d670efe1ce968d
Marty-A <henkashasmah@gmail.com>
2022-12-22 20:24:06 -0800

    p_a_s

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,16 @@ char *_strncpy(char *dest, char *src, int n)
 	int x = 0;
 
 	while (x < n && src[x] != '\0')
+	{
 		dest[x] = src[x];
 		x++;
+	}
 
 	while (x < n)
+	{
 		dest[x] = '\0';
 		x++;
+	}
 
 	return (dest);
 }

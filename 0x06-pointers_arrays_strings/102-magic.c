4388f2e073ac397c5554ff45cfd1639219d25764
Marty-A <henkashasmah@gmail.com>
2023-01-03 05:26:07 -0800

    p_a_s

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,6 +13,14 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+	/*
+	 * Write your code here
+	 * Note:
+	 * Don't use a
+	 * Don't modify p
+	 * Write only one statement
+	 * Don't add any other code
+	 */
 
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2]);

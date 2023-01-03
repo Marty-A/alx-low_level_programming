0717debf3b3f94aa230f997cb908583d7fe93521
Marty-A <henkashasmah@gmail.com>
2023-01-03 05:23:21 -0800

    p_a_s

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-	int n, a[5], *p;
+	int n;
+	int a[5];
+	int *p;
 
 	a[2] = 1024;
 	p = &n;
 
 	*(p + 5) = 98;
-	/* Prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 
 	return (0);
